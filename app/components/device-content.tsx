90d377b84595ee7f305018e228e74f77da023b68
Akihiro Saiki <misly.lx00@gmail.com>
2022-04-01 01:20:28 +0900

    improve device page

--- app/components/device-content.tsx
+++ app/components/device-content.tsx
@@ -1,27 +1,29 @@
 import NextLink from 'next/link'
 import Image from 'next/image'
-import { Box, Text, LinkBox, LinkOverlay, Heading } from '@chakra-ui/react'
+import { Box, Text, LinkBox, LinkOverlay, Heading, SimpleGrid } from '@chakra-ui/react'
 import { Global } from '@emotion/react'
 
 export const DeviceContent = ({ children, href, title, thumbnail }: { children: any, href: string, title: string, thumbnail: StaticImageData }) => (
-  <Box display="flex" w="100%" alignItems="center" py={2}>
+  <Box display="flex" w="100%" alignItems="center">
     <NextLink href={href}>
       <LinkBox display="flex" cursor="pointer" justifyContent="center">
-        <Box display="flex" pr={10}>
-          <Image src={thumbnail}
-            alt={title}
-            className="grid-contents"
-            placeholder="blur"
-            loading="lazy"
-          />
-        </Box>
-        <Box display="flex" alignItems="center" pl={5}>
-          <Heading as="h3" fontSize={23}>
-            <LinkOverlay href={href} target="_blank">
-              {children}
-            </LinkOverlay>
-          </Heading>
-        </Box>
+        <SimpleGrid columns={[2,2,2]} gap={3} justifyContent="center">
+          <Box display="flex" alignItems="center" px={3}>
+            <Image src={thumbnail}
+              alt={title}
+              className="grid-contents"
+              placeholder="blur"
+              loading="lazy"
+            />
+          </Box>
+          <Box display="flex" alignItems="center" px={3} justifyContent="center">
+            <Heading as="h3" fontSize={18}>
+              <LinkOverlay href={href} target="_blank">
+                {children}
+              </LinkOverlay>
+            </Heading>
+          </Box>
+        </SimpleGrid>
       </LinkBox>
     </NextLink>
   </Box>
--- app/pages/devices.tsx
+++ app/pages/devices.tsx
@@ -7,6 +7,10 @@ import Layout from '../components/layouts/article'
 import { DeviceContent } from '../components/device-content'
 
 import devicePc from '../public/images/devices/pc.png'
+import deviceKey from '../public/images/devices/keyboard.png'
+import deviceMouse from '../public/images/devices/mouse.png'
+import deviceMob from '../public/images/devices/mobile.png'
+import deviceAudio from '../public/images/devices/audio.png'
 
 const Devices: NextPage = () => {
   return (
@@ -17,8 +21,36 @@ const Devices: NextPage = () => {
         </Heading>
         <SimpleGrid columns={[1, 1, 1]} gap={6} mb={6}>
           <Section delay="0.1">
-            <DeviceContent href="/devices/pc" title="PCs" thumbnail={devicePc}>
+            <DeviceContent href="/devices/pc" title="PC" thumbnail={devicePc}>
               Computer
+            </DeviceContent>
+          </Section>
+        </SimpleGrid>
+        <SimpleGrid columns={[1, 1, 1]} gap={6} mb={6}>
+          <Section delay="0.1">
+            <DeviceContent href="/devices/key" title="Keyboard" thumbnail={deviceKey}>
+              Keyboard
+            </DeviceContent>
+          </Section>
+        </SimpleGrid>
+        <SimpleGrid columns={[1, 1, 1]} gap={6} mb={6}>
+          <Section delay="0.1">
+            <DeviceContent href="/devices/mouse" title="Mouse" thumbnail={deviceMouse}>
+              Mouse
+            </DeviceContent>
+          </Section>
+        </SimpleGrid>
+        <SimpleGrid columns={[1, 1, 1]} gap={6} mb={6}>
+          <Section delay="0.1">
+            <DeviceContent href="/devices/mobile" title="Mobile" thumbnail={deviceMob}>
+              Mobile
+            </DeviceContent>
+          </Section>
+        </SimpleGrid>
+        <SimpleGrid columns={[1, 1, 1]} gap={6} mb={6}>
+          <Section delay="0.1">
+            <DeviceContent href="/devices/other" title="Other" thumbnail={deviceAudio}>
+              Other
             </DeviceContent>
           </Section>
         </SimpleGrid>
